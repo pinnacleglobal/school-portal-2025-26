d71155076d082b637c4ab44deff6b62fafee9852
PGS <mittaljai01@gmail.com>
2026-03-06 05:37:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,165 +1,144 @@
-const SHEET_ID="1TBykyZx-eRMBDrRGBGGA8p_49iHlVDKN3wt9wijHJWM";
-const API_KEY="AIzaSyB5VIy4kIySW7bVrjNYMpL5rkqZ7Oe758E";
+const sheetID = "1TBykyZx-eRMBDrRGBGGA8p_49iHlVDKN3wt9wijHJWM";
+const apiKey = "AIzaSyB5VIy4kIySW7bVrjNYMpL5rkqZ7Oe758E";
 
-async function getSheet(name){
+const masterSheet = "Master Data 25 (New)";
+const feesSheet = "Fees Collection";
+const awSheet = "AW";
 
-const url=`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${name}?key=${API_KEY}`;
+function login(){
 
-const res=await fetch(url);
+let adm = document.getElementById("admno").value;
 
-const data=await res.json();
-
-return data.values || [];
-
-}
-
-async function login(){
-
-const code=document.getElementById("code").value.trim();
-
-if(!code){
-document.getElementById("msg").innerText="Enter Login Code";
+if(adm==""){
+alert("Enter Admission Number");
 return;
 }
 
-document.getElementById("loader").style.display="flex";
+document.getElementById("loginBox").style.display="none";
+document.getElementById("loader").style.display="block";
 
-const aw=await getSheet("AW");
+loadStudent(adm);
 
-let admission="";
-let name="";
+}
 
-for(let i=1;i<aw.length;i++){
 
-if(aw[i][29] && aw[i][29]==code){
+async function loadStudent(adm){
 
-admission=aw[i][1];
-name=aw[i][3];
+/* MASTER DATA */
+
+let masterURL=`https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${masterSheet}?key=${apiKey}`;
+let masterData=await fetch(masterURL).then(res=>res.json());
+
+let rows=masterData.values;
+
+let student=null;
+
+for(let i=1;i<rows.length;i++){
+
+if(rows[i][0]==adm){
+student=rows[i];
+break;
+}
+
+}
+
+if(!student){
+alert("Student not found");
+location.reload();
+return;
+}
+
+/* PROFILE DATA */
+
+document.getElementById("studentName").innerText="Welcome "+student[1];
+
+document.getElementById("class").innerText="Class : "+student[2];
+
+document.getElementById("adm").innerText="Admission No : "+adm;
+
+
+/* AW DATA */
+
+let awURL=`https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${awSheet}?key=${apiKey}`;
+let awData=await fetch(awURL).then(res=>res.json());
+
+let awRows=awData.values;
+
+for(let i=1;i<awRows.length;i++){
+
+if(awRows[i][1]==adm){
+
+document.getElementById("photo").src=awRows[i][59];
+
+document.getElementById("father").innerText="Father : "+awRows[i][6];
+
+document.getElementById("mother").innerText="Mother : "+awRows[i][5];
+
+document.getElementById("phone").innerText="Phone : "+awRows[i][22];
+
+document.getElementById("address").innerText="Address : "+awRows[i][7];
+
 break;
 
 }
 
 }
 
-if(!admission){
 
-document.getElementById("loader").style.display="none";
-document.getElementById("msg").innerText="Invalid Code";
-return;
+/* FEES DATA */
 
-}
+let feesURL=`https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${feesSheet}?key=${apiKey}`;
+let feesData=await fetch(feesURL).then(res=>res.json());
 
-loadDashboard(admission,name);
-
-}
-
-async function loadDashboard(admission,name){
-
-const master=await getSheet("Master Data 25 (New)");
-
-let studentClass="";
-
-for(let i=1;i<master.length;i++){
-
-if(master[i][1] && master[i][1]==admission){
-
-studentClass=master[i][13];
-break;
-
-}
-
-}
-
-const fees=await getSheet("Fees Collection");
+let feeRows=feesData.values;
 
 let table="";
-let cards="";
 
-for(let i=1;i<fees.length;i++){
+for(let i=1;i<feeRows.length;i++){
 
-if(fees[i][2] && fees[i][2]==admission){
+if(feeRows[i][2]==adm){
 
-let date=fees[i][1]||"NA";
-let slip=fees[i][0]||"NA";
-let deposit=fees[i][5]||"NA";
-let type=fees[i][6]||"NA";
-let session=fees[i][7]||"NA";
-let tuition=clean(fees[i][8]);
-let transport=clean(fees[i][9]);
-let exam=clean(fees[i][10]);
-let payment=fees[i][11]||"NA";
+let tuition=feeRows[i][8] || "NA";
+let transport=feeRows[i][9] || "NA";
+let exam=feeRows[i][10] || "NA";
 
 table+=`
 <tr>
-<td>${date}</td>
-<td>${slip}</td>
-<td>${deposit}</td>
-<td>${type}</td>
-<td>${session}</td>
+
+<td>${feeRows[i][1]}</td>
+
+<td>${feeRows[i][0]}</td>
+
+<td>${feeRows[i][5]}</td>
+
+<td>${feeRows[i][6]}</td>
+
+<td>${feeRows[i][7]}</td>
+
 <td>${tuition}</td>
+
 <td>${transport}</td>
+
 <td>${exam}</td>
-<td>${payment}</td>
+
 </tr>
 `;
 
-cards+=`
-
-<div class="feeCard">
-
-<b>Date:</b> ${date}<br>
-<b>Slip No:</b> ${slip}<br>
-<b>Fees Deposited:</b> ${deposit}<br>
-<b>Fee Type:</b> ${type}<br>
-<b>Session:</b> ${session}<br>
-<b>Tuition Months:</b> ${tuition}<br>
-<b>Transport Months:</b> ${transport}<br>
-<b>Exam Months:</b> ${exam}<br>
-<b>Payment Mode:</b> ${payment}
-
-</div>
-
-`;
-
 }
 
 }
 
-document.getElementById("feesTable").innerHTML=table;
-document.getElementById("cardContainer").innerHTML=cards;
-
-document.getElementById("welcome").innerText="Welcome "+name;
-
-document.getElementById("admission").innerText=admission;
-
-document.getElementById("class").innerText=studentClass;
-
-document.getElementById("loginPage").classList.add("hidden");
-
-document.getElementById("dashboard").classList.remove("hidden");
+document.getElementById("feeTable").innerHTML=table;
 
 document.getElementById("loader").style.display="none";
 
-}
-
-function clean(v){
-
-if(!v) return "NA";
-
-v=v.trim();
-
-if(v.toLowerCase()=="no") return "NA";
-
-return v;
+document.getElementById("portal").style.display="block";
 
 }
+
 
 function logout(){
 
-document.getElementById("dashboard").classList.add("hidden");
-
-document.getElementById("loginPage").classList.remove("hidden");
-
-document.getElementById("code").value="";
+location.reload();
 
 }
