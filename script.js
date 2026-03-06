61e00c5500d93f49536fe26f7576390fb2e2e64c
PGS <mittaljai01@gmail.com>
2026-03-07 01:37:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,18 +14,15 @@ alert("Enter Login Code");
 return;
 }
 
-document.getElementById("loginBtn").disabled = true;
-document.getElementById("loader").style.display = "block";
+document.getElementById("loginBtn").disabled=true;
+document.getElementById("loader").style.display="block";
 
 try{
 
-/* FETCH AW SHEET */
-
-const awURL = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${awSheet}?key=${apiKey}`;
-
-const awResp = await fetch(awURL);
-const awData = await awResp.json();
-const awRows = awData.values || [];
+const awURL=`https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${awSheet}?key=${apiKey}`;
+const awResp=await fetch(awURL);
+const awData=await awResp.json();
+const awRows=awData.values||[];
 
 let admission="";
 let studentName="";
@@ -38,12 +35,12 @@ for(let i=1;i<awRows.length;i++){
 
 if(awRows[i][29] && awRows[i][29].trim()==code){
 
-admission = awRows[i][1] || "NA";
-studentName = awRows[i][3] || "NA";
-father = awRows[i][6] || "NA";
-mother = awRows[i][5] || "NA";
-phone = awRows[i][22] || "NA";
-address = awRows[i][7] || "NA";
+admission=awRows[i][1]||"NA";
+studentName=awRows[i][3]||"NA";
+father=awRows[i][6]||"NA";
+mother=awRows[i][5]||"NA";
+phone=awRows[i][22]||"NA";
+address=awRows[i][7]||"NA";
 
 break;
 
@@ -58,42 +55,32 @@ document.getElementById("loginBtn").disabled=false;
 return;
 }
 
-/* FETCH MASTER DATA */
-
-const masterURL = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${masterSheet}?key=${apiKey}`;
-
-const masterResp = await fetch(masterURL);
-const masterData = await masterResp.json();
-const masterRows = masterData.values || [];
+const masterURL=`https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${masterSheet}?key=${apiKey}`;
+const masterResp=await fetch(masterURL);
+const masterData=await masterResp.json();
+const masterRows=masterData.values||[];
 
 let studentClass="NA";
 
 for(let i=1;i<masterRows.length;i++){
-
 if(masterRows[i][1]==admission){
-studentClass = masterRows[i][13] || "NA";
+studentClass=masterRows[i][13]||"NA";
 break;
 }
-
 }
 
-/* FILL PROFILE */
+document.getElementById("studentName").innerText="Welcome, "+studentName;
+document.getElementById("class").innerText=studentClass;
+document.getElementById("adm").innerText=admission;
+document.getElementById("father").innerText=father;
+document.getElementById("mother").innerText=mother;
+document.getElementById("phone").innerText=phone;
+document.getElementById("address").innerText=address;
 
-document.getElementById("studentName").innerText = "Welcome, "+studentName;
-document.getElementById("class").innerText = "Class : "+studentClass;
-document.getElementById("adm").innerText = "Admission No : "+admission;
-document.getElementById("father").innerText = "Father's Name : "+father;
-document.getElementById("mother").innerText = "Mother's Name : "+mother;
-document.getElementById("phone").innerText = "Phone Number : "+phone;
-document.getElementById("address").innerText = "Address : "+address;
-
-/* FETCH FEES DATA */
-
-const feesURL = `https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${feesSheet}?key=${apiKey}`;
-
-const feesResp = await fetch(feesURL);
-const feesData = await feesResp.json();
-const feeRows = feesData.values || [];
+const feesURL=`https://sheets.googleapis.com/v4/spreadsheets/${sheetID}/values/${feesSheet}?key=${apiKey}`;
+const feesResp=await fetch(feesURL);
+const feesData=await feesResp.json();
+const feeRows=feesData.values||[];
 
 let table="";
 let cards="";
@@ -102,16 +89,17 @@ for(let i=1;i<feeRows.length;i++){
 
 if(feeRows[i][2]==admission){
 
-const r0 = feeRows[i][0] || "NA";
-const r1 = feeRows[i][1] || "NA";
-const r5 = feeRows[i][5] || "NA";
-const r6 = feeRows[i][6] || "NA";
-const r7 = feeRows[i][7] || "NA";
-const r8 = feeRows[i][8] || "NA";
-const r9 = feeRows[i][9] || "NA";
-const r10 = feeRows[i][10] || "NA";
+const r0=feeRows[i][0]||"NA";
+const r1=feeRows[i][1]||"NA";
+const r5=feeRows[i][5]||"NA";
+const r6=feeRows[i][6]||"NA";
+const r7=feeRows[i][7]||"NA";
+const r8=feeRows[i][8]||"NA";
+const r9=feeRows[i][9]||"NA";
+const r10=feeRows[i][10]||"NA";
+const r11=feeRows[i][11]||"NA";
 
-table += `<tr>
+table+=`<tr>
 <td>${r1}</td>
 <td>${r0}</td>
 <td>${r5}</td>
@@ -120,29 +108,27 @@ table += `<tr>
 <td>${r8}</td>
 <td>${r9}</td>
 <td>${r10}</td>
+<td>${r11}</td>
 </tr>`;
 
-cards += `<div class="fee-card">
+cards+=`<div class="fee-card">
 <div><b>Date:</b> ${r1}</div>
-<div><b>Slip No:</b> ${r0}</div>
-<div><b>Amount:</b> ${r5}</div>
+<div><b>Slip Number:</b> ${r0}</div>
+<div><b>Amount Paid:</b> ${r5}</div>
 <div><b>Fee Type:</b> ${r6}</div>
 <div><b>Session:</b> ${r7}</div>
-<div><b>Tuition Months:</b> ${r8}</div>
-<div><b>Transport Months:</b> ${r9}</div>
-<div><b>Exam Months:</b> ${r10}</div>
+<div><b>Tuition Fee Months:</b> ${r8}</div>
+<div><b>Transport Fee Months:</b> ${r9}</div>
+<div><b>Exam Fee Months:</b> ${r10}</div>
+<div><b>Payment Mode:</b> ${r11}</div>
 </div>`;
 
 }
 
 }
 
-/* INSERT DATA */
-
-document.getElementById("feeTable").innerHTML = table;
-document.getElementById("feeCards").innerHTML = cards;
-
-/* SHOW DASHBOARD */
+document.getElementById("feeTable").innerHTML=table;
+document.getElementById("feeCards").innerHTML=cards;
 
 document.getElementById("loginBox").style.display="none";
 document.getElementById("loader").style.display="none";
@@ -150,8 +136,7 @@ document.getElementById("portal").style.display="block";
 
 }catch(error){
 
-console.error(error);
-alert("Error loading data: "+error.message);
+alert("Error loading data");
 document.getElementById("loader").style.display="none";
 document.getElementById("loginBtn").disabled=false;
 
